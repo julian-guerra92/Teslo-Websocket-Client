aeff73510cb0921cde17e5379c85e006569c3d8a
Julian Guerra <julianguerra92@gmail.com>
2022-12-11 18:14:06 -0500

    Creación de app para conexión de websocets con proyecto de teslo-api-rest

---
+++ src/socket-client.ts
@@ -0,0 +1,67 @@
+import { Manager, Socket } from 'socket.io-client';
+
+let socket: Socket;
+
+export const connectToServer = (token: string) => {
+
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            authentication: token
+        }
+    });
+    socket?.removeAllListeners(); //Método para borrar los listernes y limpiar los sockets
+    socket = manager.socket('/');
+    addListeners();
+
+}
+
+const addListeners = () => {
+
+    const clielUl = document.querySelector('#clients-ul')!;
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+    const messagesUl = document.querySelector<HTMLUListElement>('#messages-ul')
+    const serverStatusLabel = document.querySelector('#server-status')!;
+
+    socket.on('connect', () => {
+        serverStatusLabel.innerHTML = 'Connected!';
+    });
+
+    socket.on('disconnect', () => {
+        serverStatusLabel.innerHTML = 'Disconnected!';
+    });
+
+    socket.on('clients-update', (clients: string[]) => {
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `
+                <li>${clientId}</li>
+            `
+        })
+        clielUl.innerHTML = clientsHtml;
+    });
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (messageInput.value.trim().length <= 0) return;
+        socket.emit('message-from-client', {
+            id: 'Yo!!',
+            message: messageInput.value
+        })
+        console.log(socket);
+        messageInput.value = '';
+    })
+
+    socket.on('message-from-server', (payload: { fullName: string, message: string }) => {
+        const newMessage = `
+        <li>
+            <strong>${payload.fullName}: </strong>
+            <span>${payload.message}</span>
+        </li>
+        `
+        const li = document.createElement('li');
+        li.innerHTML = newMessage;
+        messagesUl?.append(li);
+    })
+
+}
